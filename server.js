80151b01608927c069c082fde7bc56c1b017d879
Seweryn <ssevve@gmail.com>
2022-08-19 03:21:47 +0200

    Add helmet and rate limiter

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const countriesData = require('./countries.json');
 
+
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.set('trust proxy', 1);
+app.use(limiter);
+app.use(helmet());
 app.use(cors());
 
 app.get('/', (req, res) => {
